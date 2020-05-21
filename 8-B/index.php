09724cb2b3d8592ab354ec0c729c048a2fb8be73
andi <aandi30082001@gmail.com>
2020-05-21 11:14:08 +0700

    resolve

--- 8-B/index.php
+++ 8-B/index.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/css/custom.css">
 
     <title>Dumbways Course</title>
 </head>
@@ -45,7 +46,17 @@
         </div>
     </nav>
 
-
+    <div class="container section">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
 
     <script src="dist/js/jquery-3.4.1.min.js"></script>
     <script src="dist/js/popper.min.js"></script>
